97f025eb4f0a8d766b95fee92d0d4e7663d59936
Allan Winckler <allan@gavea.com>
2022-05-03 21:02:58 -0300

    feat: husky - lint-staged - eslint - prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
-import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import theme from './theme';
 import Routes from './navigation';
 
 const App: React.FC = () => {
   const navigationRef = useNavigationContainerRef();
+
   return (
     <NavigationContainer ref={navigationRef}>
       <ThemeProvider theme={theme}>
